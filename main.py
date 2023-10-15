6a6064047cfd2c1a121006013d5790ee8d46b8d7
Иван Стрельцов <vanya.streltsov@gmail.com>
2023-10-15 15:56:16 +0300

    основа

---
+++ main.py
@@ -0,0 +1,4 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget,  QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QMessageBox, QRadioButton
+ 
+app = QApplication([])
---
+++ memo_main.py
@@ -0,0 +1,9 @@
+from main import *
+
+main_win = QWidget()
+
+main_win.setWindowTitle("Визначник переможця")
+main_win.setFixedSize(400, 200)
+
+main_win.show()
+app.exec()
